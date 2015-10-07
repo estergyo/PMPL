db593793a125a726a3cd0f4508548698b68a25b3
ester gyo fanny <estergyofanny@gmail.com>
2015-10-07 10:40:56 +0000

    use bootstrap to improve layout

--- lists/views.py
+++ lists/views.py
@@ -4,15 +4,7 @@ from lists.models import Item, List
 
 # Create your views here.
 def home_page(request):	
-	items = Item.objects.all()
-
-	if Item.objects.count() == 0:
-		comment = 'yey, waktunya berlibur'
-	elif Item.objects.count() < 5:
-		comment = 'sibuk tapi santai'
-	else:
-		comment = 'oh tidak'
-
+	comment = 'yey, waktunya berlibur'
 	return render(request, 'home.html', {'comment': comment})
 
 def view_list(request, list_id):
@@ -28,3 +20,16 @@ def add_item(request, list_id):
 	list_ = List.objects.get(id=list_id)
 	Item.objects.create(text=request.POST['item_text'], list=list_)
 	return redirect('/lists/%d/' % (list_.id,))
+
+def view_list(request, list_id):
+	list_ = List.objects.get(id=list_id)
+	count_list = Item.objects.filter(list_id=list_id).count()
+
+	comment = ""
+	if count_list == 0:
+		comment = "Yey, waktunya berlibur"
+	if count_list < 5:
+		comment = "Sibuk tapi santai"
+	else:
+		comment = "Oh tidak"
+	return render(request, 'list.html', {'list': list_, 'comment': comment})
